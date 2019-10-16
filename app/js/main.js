4d66f5d2a34c00c760f3c4dc2f6a98592c49a34e
rodrigomartinezar <rodrigomartinezaraya97@gmail.com>
2019-10-16 12:51:40 -0300

    format: mejor visualizacion del codigo js

--- app/js/main.js
+++ app/js/main.js
@@ -142,7 +142,13 @@ const createTable = (data) => {
 				var content = modal.querySelector('p');
 
 				content.innerHTML = '<h1><b>Información</b></h1><br>' +
-									'<ul> <li> Nombre: ' + row.cells.item(0).innerHTML + ' ' + row.cells.item(1).innerHTML + '</li><li> Edad: ' + row.cells.item(2).innerHTML + '</li><li> PSU Matemática: ' + row.cells.item(3).innerHTML + '</li><li> PSU Lenguaje: ' + row.cells.item(4).innerHTML + '</li><li> Puntaje Rankink: ' + row.cells.item(5).innerHTML + '</li><li> Puntaje NEM: ' + row.cells.item(6).innerHTML + '</li><li>Correo: ' + row.cells.item(7).innerHTML + '</li></ul>';
+									'<ul> <li> Nombre: ' + row.cells.item(0).innerHTML + ' ' + row.cells.item(1).innerHTML +
+									'</li><li> Edad: ' + row.cells.item(2).innerHTML +
+									'</li><li> PSU Matemática: ' + row.cells.item(3).innerHTML +
+									'</li><li> PSU Lenguaje: ' + row.cells.item(4).innerHTML +
+									'</li><li> Puntaje Rankink: ' + row.cells.item(5).innerHTML +
+									'</li><li> Puntaje NEM: ' + row.cells.item(6).innerHTML +
+									'</li><li>Correo: ' + row.cells.item(7).innerHTML + '</li></ul>';
 				content.style.textAlign = 'left';
 
 				modal.style.display = 'block';
